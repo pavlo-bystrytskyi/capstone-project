ff666e3f843a026fe83ccd09d65f20507f2b805e
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-10-30 16:26:32 +0100

    TASK-17: Implement Wishlist Item update endpoint

--- backend/src/main/java/org/example/backend/repository/ItemRepository.java
+++ backend/src/main/java/org/example/backend/repository/ItemRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface ItemRepository extends MongoRepository<Item, String> {
+
     Optional<Item> findByPublicId(String publicId);
 }
