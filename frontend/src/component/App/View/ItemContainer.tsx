914db6b5457ff704a24e32ed140f2a2d5ccb30b6
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-11-07 17:18:05 +0100

    TASK-39: Implement registry item public editing component

--- frontend/src/component/App/View/ItemContainer.tsx
+++ frontend/src/component/App/View/ItemContainer.tsx
@@ -1,6 +1,3 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-import Item from "../../../type/Item.tsx";
 import ItemComponent from "./ItemContainer/ItemComponent.tsx";
 import ViewConfig from "../../../type/ViewConfig.tsx";
 
@@ -13,32 +10,11 @@ export default function ItemContainer(
         config: ViewConfig
     }
 ) {
-    const [itemList, setItemList] = useState<Item[]>([])
-    const loadItems = async function () {
-        if (!itemIdList) return;
-        try {
-            const items = await Promise.all(
-                itemIdList.map(
-                    async (itemId: string) => (await axios.get<Item>(config.item.url + itemId)).data
-                )
-            );
-            setItemList(items);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-    useEffect(
-        () => {
-            loadItems();
-        },
-        [itemIdList]
-    );
-
     return <>
         <h2>Item Container</h2>
         {
-            itemList.map(
-                (item) => <ItemComponent key={item.publicId} item={item} config={config}/>
+            itemIdList.map(
+                (itemId) => <ItemComponent key={itemId} itemId={itemId} config={config}/>
             )
         }
     </>
--- frontend/src/component/App/View/ItemContainer/ItemComponent.tsx
+++ frontend/src/component/App/View/ItemContainer/ItemComponent.tsx
@@ -2,30 +2,39 @@ import Item from "../../../../type/Item.tsx";
 import {useTranslation} from "react-i18next";
 import ItemStatus from "../../../../type/ItemStatus.tsx";
 import ViewConfig from "../../../../type/ViewConfig.tsx";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import axios from "axios";
 
 export default function ItemComponent(
     {
-        item,
+        itemId,
         config
     }: {
-        item: Item,
+        itemId: string,
         config: ViewConfig
     }
 ) {
     const {t} = useTranslation();
     const editStatusAllowed = config.access.item.status.edit;
-    const [itemState, setItemState] = useState<Item>(item)
-
+    const [item, setItem] = useState<Item>()
+    const loadItem = function () {
+        axios.get<Item>(
+            config.item.url + itemId
+        ).then(
+            result => setItem(result.data)
+        ).catch(
+            error => console.error('Error fetching data:', error)
+        );
+    }
+    useEffect(
+        loadItem, [itemId]
+    );
     const handleStatusChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        if (!item) return;
         const {value} = event.target;
-        setItemState((prevState) => ({
-            ...prevState,
-            status: value as ItemStatus,
-        }));
-        axios.put<Item>(config.item.url + item[config.item.idField as keyof Item], itemState).then(
-            (response) => setItemState(response.data)
+        item.status = value as ItemStatus;
+        axios.put<Item>(config.item.url + item[config.item.idField as keyof Item], item).then(
+            (response) => setItem(response.data)
         ).catch(error => {
             console.error('Error fetching data:', error);
         });
@@ -33,18 +42,19 @@ export default function ItemComponent(
 
     return <form className="item-form">
         <label htmlFor="title">{t("item_name")}</label>
-        <input type="text" name="title" disabled={true} value={itemState.product.title}/>
+        <input type="text" name="title" disabled={true} value={item?.product.title}/>
         <label htmlFor="description">{t("item_description")}</label>
-        <input type="text" name="description" disabled={true} value={itemState.product.description}/>
+        <input type="text" name="description" disabled={true} value={item?.product.description}/>
         <label htmlFor="link">{t("item_link")}</label>
-        <input type="text" name="link" disabled={true} value={itemState.product.link}/>
+        <input type="text" name="link" disabled={true} value={item?.product.link}/>
         <label htmlFor="status">{t("item_status")}</label>
-        <select name="status" disabled={!editStatusAllowed} onChange={handleStatusChange}>
+        <select name="status" disabled={!editStatusAllowed} onChange={handleStatusChange}
+                value={item?.status}>
             <option value={ItemStatus.AVAILABLE}>{t("status_available")}</option>
             <option value={ItemStatus.RESERVED}>{t("status_reserved")}</option>
             <option value={ItemStatus.PURCHASED}>{t("status_purchased")}</option>
         </select>
         <label htmlFor="quantity">{t("item_quantity")}</label>
-        <input type="text" name="quantity" disabled={true} value={itemState.quantity}/>
+        <input type="text" name="quantity" disabled={true} value={item?.quantity}/>
     </form>
 }
